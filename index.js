823c1f671e6c4ece1065c5c015fb4cce1dcb2588
Jader Golindano <golindano.jader@gmail.com>
2024-11-14 10:15:34 -0400

    desabilitar el certificado https

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {extract} from '@extractus/article-extractor'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; //desabilitar el certificado https
 let web = await extract('https://www.bcv.org.ve/')  
 const titulo = "tasa BCV: "
 var output = JSON.stringify(web)
